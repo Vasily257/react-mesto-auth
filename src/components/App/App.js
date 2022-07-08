2926a442e16a7a5bb8d45e6120195b51298acffd
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2022-07-08 11:05:11 +0400

    feat: move a part of functions to the Main from the App, add cards context

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { CardsContext } from '../../contexts/CardsContext';
 
 import Header from '../Header/Header';
 import Home from '../Home/Home';
@@ -8,27 +9,8 @@ import Register from '../Register/Register';
 
 import Spinner from '../Spinner/Spinner';
 
-import { api } from '../../utils/api';
-
 function App() {
-  const [currentUser, setCurrentUser] = useState({
-    name: 'Загрузка...',
-    about: 'Загрузка...',
-    avatar: 'https://dummyimage.com/120',
-  });
-
   const [cards, setCards] = useState([]);
-
-  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
-  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
-  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
-
-  const [isConfirmActionPopupOpen, setIsConfirmActionPopupOpen] =
-    useState(false);
-
-  const [selectedCard, setSelectedCard] = useState(null);
-  const [deletedCard, setDeletedCard] = useState(null);
-
   const [isSpinnerShown, setIsSpinnerShown] = useState(false);
 
   const [headerText, setHeaderText] = useState({
@@ -37,119 +19,7 @@ function App() {
     exit: '',
   });
 
-  function handleEditProfileClick() {
-    setIsEditProfilePopupOpen(true);
-  }
-
-  function handleAddPlaceClick() {
-    setIsAddPlacePopupOpen(true);
-  }
-
-  function handleEditAvatarClick() {
-    setIsEditAvatarPopupOpen(true);
-  }
-
-  function handleCardClick(currentCard) {
-    setSelectedCard(currentCard);
-  }
-
-  function handleCardLike(card) {
-    const isLiked = card.likes.some((like) => like._id === currentUser._id);
-
-    api
-      .changeLikeCardStatus(card._id, isLiked)
-      .then((newCard) => {
-        setCards((prevState) =>
-          prevState.map((prevCard) => {
-            return prevCard._id === card._id
-              ? { ...newCard, key: card._id }
-              : { ...prevCard, key: prevCard._id };
-          })
-        );
-      })
-      .catch((error) => console.log(`Ошибка: ${error}`));
-  }
-
-  function deleteCard(card) {
-    setIsSpinnerShown(true);
-
-    api
-      .deleteCard(card._id)
-      .then(() => {
-        setCards((prevState) => {
-          return prevState.filter((prevCard) => prevCard._id !== card._id);
-        });
-        closeAllPopups();
-      })
-      .catch((error) => console.log(`Ошибка: ${error}`))
-      .finally(() => {
-        setIsSpinnerShown(false);
-      });
-  }
-
-  function handleCardDelete(deletedCard) {
-    setIsConfirmActionPopupOpen(true);
-    setDeletedCard(deletedCard);
-  }
-
-  function closeAllPopups() {
-    setIsEditProfilePopupOpen(false);
-    setIsAddPlacePopupOpen(false);
-    setIsEditAvatarPopupOpen(false);
-    setIsConfirmActionPopupOpen(false);
-
-    setSelectedCard(null);
-    setDeletedCard(null);
-  }
-
-  function handleUpdateUser(userInfo) {
-    setIsSpinnerShown(true);
-
-    api
-      .editUserInfo(userInfo)
-      .then((resWithUserInfo) => {
-        setCurrentUser(resWithUserInfo);
-        closeAllPopups();
-      })
-      .catch((error) => console.log(`Ошибка: ${error}`))
-      .finally(() => {
-        setIsSpinnerShown(false);
-      });
-  }
-
-  function handleUpdateAvatar(avatar) {
-    setIsSpinnerShown(true);
-
-    api
-      .updateAvatar(avatar)
-      .then((resWithAvatar) => {
-        setCurrentUser(resWithAvatar);
-        closeAllPopups();
-      })
-      .catch((error) => console.log(`Ошибка: ${error}`))
-      .finally(() => {
-        setIsSpinnerShown(false);
-      });
-  }
-
-  function handleAddPlaceSubmit(popupData) {
-    setIsSpinnerShown(true);
-
-    api
-      .addNewCard(popupData)
-      .then((newCard) => {
-        setCards([{ ...newCard, key: newCard._id }, ...cards]);
-        closeAllPopups();
-      })
-      .catch((error) => console.log(`Ошибка: ${error}`))
-      .finally(() => {
-        setIsSpinnerShown(false);
-      });
-  }
-
   useEffect(() => {
-    setIsSpinnerShown(true);
-
     switch (window.location.pathname) {
       case '/sign-up':
         setHeaderText({
@@ -180,69 +50,30 @@ function App() {
         });
         break;
     }
-
-    api
-      .getInitialData()
-      .then((initialData) => {
-        const [userData, initialCardsData] = initialData;
-
-        setCurrentUser(userData);
-
-        setCards(
-          initialCardsData.map((item) => ({
-            name: item.name,
-            link: item.link,
-            likes: item.likes,
-            owner: item.owner,
-            _id: item._id,
-          }))
-        );
-      })
-      .catch((error) => console.log(`Ошибка: ${error}`))
-      .finally(() => setIsSpinnerShown(false));
   }, []);
 
   return (
     <div className="page index-page">
       <Header text={headerText} />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              currentUser={currentUser}
-              isEditProfilePopupOpen={isEditProfilePopupOpen}
-              isAddPlacePopupOpen={isAddPlacePopupOpen}
-              isEditAvatarPopupOpen={isEditAvatarPopupOpen}
-              isConfirmActionPopupOpen={isConfirmActionPopupOpen}
-              cards={cards}
-              selectedCard={selectedCard}
-              deletedCard={deletedCard}
-              handleEditProfileClick={handleEditProfileClick}
-              handleAddPlaceClick={handleAddPlaceClick}
-              handleEditAvatarClick={handleEditAvatarClick}
-              handleCardClick={handleCardClick}
-              handleCardLike={handleCardLike}
-              handleCardDelete={handleCardDelete}
-              deleteCard={deleteCard}
-              closeAllPopups={closeAllPopups}
-              handleUpdateUser={handleUpdateUser}
-              handleUpdateAvatar={handleUpdateAvatar}
-              handleAddPlaceSubmit={handleAddPlaceSubmit}
-            />
-          }
-        />
 
-        <Route
-          path="/sign-up"
-          element={<Register isOpen={true} onLogin={{}} />}
-        />
-        <Route
-          path="/sign-in"
-          element={<Login isOpen={true} onRegister={{}} />}
-        />
-        <Route path="*" element={<div>Страница не найдена. Код 404</div>} />
-      </Routes>
+      <CardsContext.Provider value={{ cards, setCards }}>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home setIsSpinnerShown={setIsSpinnerShown} />}
+          />
+          <Route
+            path="/sign-up"
+            element={<Register isOpen={true} onLogin={{}} />}
+          />
+          <Route
+            path="/sign-in"
+            element={<Login isOpen={true} onRegister={{}} />}
+          />
+          <Route path="*" element={<div>Страница не найдена. Код 404</div>} />
+        </Routes>
+      </CardsContext.Provider>
+
       <Spinner isOpen={isSpinnerShown} />
     </div>
   );
